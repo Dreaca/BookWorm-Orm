f0e67467e1c09ff41a9b5e5f6f818942ef516989
dreaca <buddhikadreaca@gmail.com>
2024-03-07 12:27:11 +0530

    Initial commit

---
+++ src/main/java/org/example/controller/LoginController.java
@@ -0,0 +1,26 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpOnAction(ActionEvent event) {
+
+    }
+
+}
